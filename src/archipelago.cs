f75280f239f82081cf687e8b8040c5fb4406bb27
evelyntsmg <evelyntsmg@proton.me>
2026-03-05 13:03:41 +0100

    Combine Contest of Aeons settings

--- src/archipelago.cs
+++ src/archipelago.cs
@@ -135,9 +135,7 @@ public unsafe partial class ArchipelagoFFXModule : FhModule {
         [JsonInclude]
         public int             CaptureDamage;
         [JsonInclude]
-        public int             SkipContestOfAeons;
-        [JsonInclude]
-        public int             HardcoreContestOfAeons;
+        public int             ContestOfAeonsDifficulty;
 
         public ArchipelagoSeedOptions() {
             PlayerName           = "";
@@ -942,7 +940,7 @@ public unsafe partial class ArchipelagoFFXModule : FhModule {
 
     public static void try_remove_autolife() {
         // In every Contest of Aeons battle, autolife is applied at 004E through 005A, so just remove those
-        if (seed.Options.HardcoreContestOfAeons != 0) {
+        if (seed.Options.ContestOfAeonsDifficulty == 2) {
             AtelScriptChunk* script_chunk = Globals.Atel.controllers[0].script_chunk;
             nint code_ptr = (nint)((int)script_chunk + script_chunk->offset_code);
             NativeMemory.Fill((void*)(code_ptr + 0x4E), 0xC, 0);
