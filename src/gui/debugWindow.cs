fcd1556496cda7bb76118bb3183b2c0f7d59a07c
evelyntsmg <evelyntsmg@proton.me>
2026-03-06 19:50:29 +0100

    Autoremove whitespace

--- src/gui/debugWindow.cs
+++ src/gui/debugWindow.cs
@@ -67,7 +67,7 @@ public unsafe static class ArchipelagoGUI {
     public static int font_size = -1;
 
     private static bool show_popup;
-    public static string popup_content { 
+    public static string popup_content {
         get;
         set {
             field = value;
@@ -88,7 +88,7 @@ public unsafe static class ArchipelagoGUI {
         if (font_size == -1) font_size = (int)ImGui.GetFontSize();
         ImGui.PushFont(null, font_size);
 
-        if (show_popup) { 
+        if (show_popup) {
             ImGui.OpenPopup("Archipelago.GUI.Popup");
             show_popup = false;
         }
@@ -384,7 +384,7 @@ public unsafe static class ArchipelagoGUI {
 
                         ImGui.EndTable();
                     }
-                    
+
                 }
             }
             //var localizationManager = _LocalizationManager_GetInstance();
@@ -475,7 +475,7 @@ public unsafe static class ArchipelagoGUI {
         //    SphereGridTilt.SlightTilt => 1.125f,
         //    _ => 1,
         //};
-        
+
         float main_x = 2560;
         float main_y = 1440;
         float x_ratio = main_x / ImGui.GetWindowViewport().Size.X;
@@ -831,13 +831,13 @@ public unsafe static class ArchipelagoGUI {
                                     List<(string, Color)> message = [("invalid map_id: ", Color.Red), (mapString, Color.Blue)];
                                     add_log_message(message);
                                 }
-                    
+
                             }
                             else {
                                 List<(string, Color)> message = [("invalid story_progress: ", Color.Red), (progressString, Color.Blue)];
                                 add_log_message(message);
                             }
-                    
+
                         }
                         else {
                             List<(string, Color)> message = [("invalid region: ", Color.Red), (regionString, Color.Blue)];
@@ -883,7 +883,7 @@ public unsafe static class ArchipelagoGUI {
                     ,
                     ["/clear"] => () => {
                         lock (client_log_lock) {
-                            client_log.Clear(); 
+                            client_log.Clear();
                         }
                     }
                     ,
@@ -908,7 +908,7 @@ public unsafe static class ArchipelagoGUI {
             client_input_command = "";
         }
     }
-    
+
     private static void render_debug_tab() {
 #if DEBUG
         fixed (int* ap_mult = &ArchipelagoFFXModule.ap_multiplier) {
@@ -1099,7 +1099,7 @@ public unsafe static class ArchipelagoGUI {
                     ImGui.SeparatorText("Excess");
                     if (excess_inventory.Count == 0) {
                         ImGui.Text("Empty");
-                    } else { 
+                    } else {
                         foreach ((uint item_id, int amount) in excess_inventory) {
                             if (amount == 0) continue;
                             string item_name = get_item_name(item_id);
