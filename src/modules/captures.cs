2e857acb3089916f0b4085d6a6ea2e9ca465d6ca
evelyntsmg <evelyntsmg@proton.me>
2026-04-01 16:09:59 +0200

    Initial `MsBattleEncountExe` reimplementation for encounter weighing

--- src/modules/captures.cs
+++ src/modules/captures.cs
@@ -1,10 +1,14 @@
-﻿using Archipelago.MultiClient.Net.Enums;
+﻿using System;
+
+using Archipelago.MultiClient.Net.Enums;
 using Fahrenheit.Atel;
 using Fahrenheit.FFX;
 using Fahrenheit.FFX.Battle;
 using Fahrenheit.Modules.ArchipelagoFFX.Client;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using static Fahrenheit.FFX.Globals;
 using static Fahrenheit.Modules.ArchipelagoFFX.Client.FFXArchipelagoClient;
@@ -14,6 +18,50 @@ namespace Fahrenheit.Modules.ArchipelagoFFX;
 
 [FhLoad(FhGameId.FFX)]
 public unsafe partial class CaptureModule : FhModule {
+    // Some structs that Fahrenheit is currently missing
+    [StructLayout(LayoutKind.Sequential)]
+    private struct BtlBinField {
+        [InlineArray(8)]
+        public struct FieldName {
+            private char _data;
+        }
+
+        public short id;
+        public short data_offset;
+        public short __0x4;
+        public FieldName name;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x2)]
+    private struct BtlBinEncounter {
+        [FieldOffset(0x0)] public byte total_formation_count;
+        [FieldOffset(0x1)] public byte group_count;
+
+        [FieldOffset(0x2)] private BtlBinGroup _first_group;
+
+        [UnscopedRef]
+        public Span<BtlBinGroup> groups => MemoryMarshal.CreateSpan(ref _first_group, group_count);
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x5)]
+    private struct BtlBinGroup {
+        [FieldOffset(0x0)] public byte formation_count;
+        [FieldOffset(0x1)] public short battlefield;
+        [FieldOffset(0x3)] public byte grace;
+        [FieldOffset(0x4)] public byte total_weight;
+
+        [FieldOffset(0x5)] private BtlBinFormation _first_formation;
+
+        [UnscopedRef]
+        public Span<BtlBinFormation> formations => MemoryMarshal.CreateSpan(ref _first_formation, formation_count);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct BtlBinFormation {
+        public byte index;
+        public byte weight;
+    }
+
     private FhModContext? _mod_context;
     private FileStream? _global_state;
 
@@ -28,6 +76,7 @@ public unsafe partial class CaptureModule : FhModule {
         _MsSetRamChrParam = new FhMethodHandle<MsSetRamChrParam>(this, GAME, __addr_MsSetRamChrParam, h_MsSetRamChrParam);
         _MsSetSaveParam = new FhMethodHandle<MsSetSaveParam>(this, GAME, __addr_MsSetSaveParam, h_MsSetSaveParam);
         _MsCalcCommand = new FhMethodHandle<MsCalcCommand>(this, GAME, __addr_MsCalcCommand, h_MsCalcCommand);
+        _MsBattleEncountExe = new FhMethodHandle<MsBattleEncountExe>(this, GAME, __addr_MsBattleEncountExe, h_MsBattleEncountExe);
     }
 
     public override bool init(FhModContext mod_context, FileStream global_state_file) {
@@ -41,7 +90,8 @@ public unsafe partial class CaptureModule : FhModule {
             && _MsDamageCheckDeath.hook()
             && _MsSetRamChrParam.hook()
             && _MsSetSaveParam.hook()
-            && _MsCalcCommand.hook();
+            && _MsCalcCommand.hook()
+            && _MsBattleEncountExe.hook();
     }
 
     private static void set(byte* code_ptr, uint offset, AtelInst[] opcodes) {
@@ -100,7 +150,7 @@ public unsafe partial class CaptureModule : FhModule {
         231, // Dingo (Tutorial)
         232, // Water Flan (Tutorial)
         233, // Condor (Tutorial)
-        234, // Ragora (Tutorial)    
+        234, // Ragora (Tutorial)
     ];
     private void h_FUN_00783bb0(byte mon_idx) {
         byte num_initialized = FhUtil.get_at<byte>(0xD2CA80);
@@ -115,7 +165,7 @@ public unsafe partial class CaptureModule : FhModule {
             // Corrects incorrect monster arena indexes to be uncapturable
             if (_incorrect_arena_idx.Contains((short)(mon->chr_id & 0xFFF))) {
                 stats->monster_arena_idx = 0xFF;
-            }                
+            }
         }
         else {
             _logger.Debug($"{mon_idx}: already initialized ({mon->chr_id})");
@@ -246,5 +296,149 @@ public unsafe partial class CaptureModule : FhModule {
             }
         }
     }
-}
 
+    private int h_MsBattleEncountExe(int field_id, int group_idx, float walked_delta) {
+        _logger.Info($"MsBattleEncountExe(0x{field_id:X}, {group_idx}, {walked_delta})");
+
+        // Globals
+        int* g_keybattle = FhUtil.ptr_at<int>(0xD2CA24);
+        int* g_keydown_R = FhUtil.ptr_at<int>(0xD2CA20);
+        int* DAT_0112ca28 = FhUtil.ptr_at<int>(0xD2CA28);
+        int* g_encounter_level = FhUtil.ptr_at<int>(0x8421C8);
+        int* EnableBattle = FhUtil.ptr_at<int>(0x8421BC);
+
+        _logger.Info($"  g_keybattle = {*g_keybattle}");
+        _logger.Info($"  g_keydown_R = {*g_keydown_R}");
+        _logger.Info($"  DAT_0112ca28 = {*DAT_0112ca28}");
+        _logger.Info($"  g_encounter_level = {*g_encounter_level}");
+        _logger.Info($"  EnableBattle = {*EnableBattle}");
+
+        int field_idx = _MsBtlListFieldNum(field_id);
+
+        if (*g_keybattle != 0 && *g_keydown_R != 0) {
+            Battle.btl->group_idx = (byte)group_idx;
+            Battle.btl->formation_idx = (byte)*DAT_0112ca28;
+            *(byte*)((nint)Battle.btl + 0x12) = 1;
+            Battle.btl->field_idx = (ushort)field_idx;
+            _ResetEncountExe(1);
+            return -1;
+        }
+
+        bool can_encounter = true;
+        if (*g_encounter_level == 2) { // x4 encounter booster
+            walked_delta *= 10.0f;
+            can_encounter = true;
+        } else if (*g_encounter_level == 1) {
+            can_encounter = *(bool*)((nint)Battle.btl + 0x107);
+        } else if (*g_encounter_level == 0) {
+            walked_delta = 0.0f;
+            can_encounter = false;
+        }
+
+        if (*(byte*)((nint)Battle.btl + 0x12) == 2) return 0;
+        if (field_idx < 0) return 0;
+        if (walked_delta <= 0.0f || !can_encounter) return 0;
+
+        BtlBinField* field = _MsBtlListField(field_idx);
+        BtlBinEncounter* encounter = _MsBtlListEncount(field_idx);
+
+        if (group_idx < 0 || group_idx >= encounter->group_count) return 0;
+        if (*EnableBattle != 0) return 0;
+
+        BtlBinGroup* group = _MsBtlListGroup(field_idx, group_idx);
+
+        _logger.Info( "  Group:");
+        _logger.Info($"    formation_count: {group->formation_count}");
+        _logger.Info($"    battlefield: {group->battlefield:X}");
+        _logger.Info($"    grace: {group->grace}");
+        _logger.Info($"    total_weight: {group->total_weight}");
+
+        if (group->grace == 0 || group->total_weight == 0) return 0;
+
+        Battle.btl->walked_dist += walked_delta;
+        Battle.btl->walked_dist_total += walked_delta;
+        Battle.btl->grace = group->grace;
+
+        int rolls_so_far = (int)(Battle.btl->walked_dist_total / 10.0f);
+
+        _logger.Info($"  rolls_so_far: {rolls_so_far}");
+        _logger.Info($"  rolls_to_do: {(int)(Battle.btl->walked_dist / 10.0f)}");
+        _logger.Info( "  Rolls:");
+
+        for (; 10.0f < Battle.btl->walked_dist; Battle.btl->walked_dist -= 10.0f) {
+            if (group->grace / 2 >= rolls_so_far) continue;
+
+            int encounter_chance = (rolls_so_far - (group->grace / 2)) * 256 / (group->grace * 4);
+            *(float*)((nint)Battle.btl + 0x110) *= 256.0f - encounter_chance;
+            *(float*)((nint)Battle.btl + 0x114) *= 256.0f;
+
+            _logger.Info($"  encounter_chance: {encounter_chance}");
+            _logger.Info($"  new btl__0x110: {*(float*)((nint)Battle.btl + 0x110)}");
+            _logger.Info($"  new btl__0x114: {*(float*)((nint)Battle.btl + 0x114)}");
+
+            if ((_brnd(0) & 0xFF) >= encounter_chance) continue;
+
+            int formation_rng = _brnd(1);
+            int iVar7 = 0;
+
+            for (int formation_idx = 0; formation_idx < group->formation_count; formation_idx++) {
+                BtlBinFormation formation = group->formations[formation_idx];
+
+                iVar7 += formation.weight / 16;
+                if (!(formation_rng % group->total_weight < iVar7)) continue;
+
+                string file_name = $"{new string(field->name)}{group_idx}_{formation_idx}";
+                _logger.Info($"  Rolled Formation #{formation_idx} ({file_name})!");
+
+                string filepath = $"host0:/ffx/master/jppc/battle/btl/{file_name}/{file_name}.bin";
+                char[] filepath_c = filepath.ToCharArray();
+
+                fixed (char* filepath_ptr = &filepath_c[0]) {
+                    void* open_result = _sceOpen(filepath_ptr, 1);
+
+                    if (open_result is null) goto IGNORE_FILE;
+
+                    int file_size = _sceLseek(open_result, 0, 2);
+                    _sceLseek(open_result, 0, 0);
+
+                    void* file = NativeMemory.Alloc((nuint)file_size);
+                    _sceRead(open_result, file, file_size);
+
+                    int chunk_ptr = *(int*)((nint)file + 0xC);
+                    if (chunk_ptr != 0) {
+                        short* mon_ids_ptr = (short*)((nint)file + chunk_ptr + 0x8);
+                        List<string> mon_ids = new(8);
+
+                        for (int i = 0; i < 8; i++) {
+                            short mon_id = mon_ids_ptr[i];
+                            if (mon_id == -1) continue;
+
+                            mon_ids.Add($"{mon_id:X4}");
+                        }
+
+                        _logger.Info($"    Monsters: {string.Join(", ", mon_ids)}");
+                    }
+
+                    _sceClose(open_result);
+                }
+
+IGNORE_FILE:
+                if (*(byte*)((nint)Battle.btl + 0x27) == 0) {
+                    *(byte*)((nint)Battle.btl + 0x12) = 1;
+                } else {
+                    _Sg_FadeInW(3);
+                    save_data->battle_count += 1;
+                }
+
+                Battle.btl->field_idx = (ushort)field_idx;
+                Battle.btl->group_idx = (byte)group_idx;
+                Battle.btl->formation_idx = (byte)formation_idx;
+                _ResetEncountExe(1);
+
+                return -1;
+            }
+        }
+
+        return 0;
+    }
+}
