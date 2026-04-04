b351a3778664785da637aee19e6059498797e962
EvelynTSMG <96205195+EvelynTSMG@users.noreply.github.com>
2026-04-04 13:40:26 +0000

    Weigh encounters with uncaptured monsters (#185)
    * Initial `MsBattleEncountExe` reimplementation for encounter weighing
    
    * Add more logs to `MsBattleEncountExe`
    
    * Only log in `MsBattleEncountExe` when relevant
    
    * Return the proper value for a no-op `MsBattleEncountExe`
    
    * Fix inversed `if` statement in `MsBattleEncountExe`
    
    * Improve logging in `MsBattleEncountExe`
    
    * Implement weighing encounters
    
    I am unable to test this properly myself
    
    * Fix trying to open incorrect formation file
    
    * Do `formation_rng % total_weight` only once
    
    * Account for capture requirements when weighing encounters.
    
    * Remove unnecessary logging, clean up code
    
    * Fix crash on weighting formations with uncapturable monsters
    
    * Do not grant extra weight to unrollable encounters
    
    No more Lord Ochu random encounters in Kilika :c
    
    * Restore vanilla weights if all formations are complete

--- src/delegates/capture_delegates.cs
+++ src/delegates/capture_delegates.cs
@@ -60,6 +60,54 @@ public unsafe partial class CaptureModule : FhModule {
 
     private const nint __addr_ret_hasKeyItem = 0x45B7A0;
 
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int MsBattleEncountExe(int field_id, int group_idx, float walked_delta);
+    public const nint __addr_MsBattleEncountExe = 0x380de0;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int MsBtlListFieldNum(int field_id);
+    public const nint __addr_MsBtlListFieldNum = 0x39d1e0;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void ResetEncountExe(int p1);
+    public const nint __addr_ResetEncountExe = 0x3810c0;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int brnd(int rng_slot);
+    public const nint __addr_brnd = 0x398900;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate BtlBinField* MsBtlListField(int field_idx);
+    private const nint __addr_MsBtlListField = 0x39d1b0;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate BtlBinEncounter* MsBtlListEncount(int field_idx);
+    private const nint __addr_MsBtlListEncount = 0x39d190;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate BtlBinGroup* MsBtlListGroup(int field_idx, int group_idx);
+    private const nint __addr_MsBtlListGroup = 0x39d230;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void Sg_FadeInW(int p1);
+    public const nint __addr_Sg_FadeInW = 0x42cc20;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void* sceOpen(byte* p1, int p2);
+    public const nint __addr_sceOpen = 0x22fbe0;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int sceLseek(void* p1, int p2, int p3);
+    public const nint __addr_sceLseek = 0x22fa90;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int sceRead(void* p1, void* dst, int amount);
+    public const nint __addr_sceRead = 0x22fdb0;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int sceClose(void* p1);
+    public const nint __addr_sceClose = 0x22f7c0;
+
     // Method Handles
     private readonly FhMethodHandle<MsMonsterCapture> _MsMonsterCapture;
     private readonly FhMethodHandle<FUN_00783bb0> _FUN_00783bb0;
@@ -69,6 +117,7 @@ public unsafe partial class CaptureModule : FhModule {
     private readonly FhMethodHandle<MsSetSaveParam> _MsSetSaveParam;
     private readonly FhMethodHandle<MsSetRamChrParam> _MsSetRamChrParam;
     private readonly FhMethodHandle<MsCalcCommand> _MsCalcCommand;
+    private readonly FhMethodHandle<MsBattleEncountExe> _MsBattleEncountExe;
 
     private static char* get_event_name(uint event_id) => FhUtil.get_fptr<AtelGetEventName>(__addr_AtelGetEventName)(event_id);
     private readonly MsGetMon _MsGetMon = FhUtil.get_fptr<MsGetMon>(__addr_MsGetMon);
@@ -76,4 +125,15 @@ public unsafe partial class CaptureModule : FhModule {
     private readonly MsGetCommand _MsGetCommand = FhUtil.get_fptr<MsGetCommand>(__addr_MsGetCommand);
     private readonly FUN_0078d100 _FUN_0078d100 = FhUtil.get_fptr<FUN_0078d100>(__addr_FUN_0078d100);
     private readonly FUN_0078bb30 _FUN_0078bb30 = FhUtil.get_fptr<FUN_0078bb30>(__addr_FUN_0078bb30);
+    private readonly MsBtlListFieldNum _MsBtlListFieldNum = FhUtil.get_fptr<MsBtlListFieldNum>(__addr_MsBtlListFieldNum);
+    private readonly ResetEncountExe _ResetEncountExe = FhUtil.get_fptr<ResetEncountExe>(__addr_ResetEncountExe);
+    private readonly brnd _brnd = FhUtil.get_fptr<brnd>(__addr_brnd);
+    private readonly MsBtlListField _MsBtlListField = FhUtil.get_fptr<MsBtlListField>(__addr_MsBtlListField);
+    private readonly MsBtlListEncount _MsBtlListEncount = FhUtil.get_fptr<MsBtlListEncount>(__addr_MsBtlListEncount);
+    private readonly MsBtlListGroup _MsBtlListGroup = FhUtil.get_fptr<MsBtlListGroup>(__addr_MsBtlListGroup);
+    private readonly Sg_FadeInW _Sg_FadeInW = FhUtil.get_fptr<Sg_FadeInW>(__addr_Sg_FadeInW);
+    private readonly sceOpen _sceOpen = FhUtil.get_fptr<sceOpen>(__addr_sceOpen);
+    private readonly sceLseek _sceLseek = FhUtil.get_fptr<sceLseek>(__addr_sceLseek);
+    private readonly sceRead _sceRead = FhUtil.get_fptr<sceRead>(__addr_sceRead);
+    private readonly sceClose _sceClose = FhUtil.get_fptr<sceClose>(__addr_sceClose);
 }
--- src/modules/captures.cs
+++ src/modules/captures.cs
@@ -1,11 +1,17 @@
-﻿using Archipelago.MultiClient.Net.Enums;
+﻿using System;
+
+using Archipelago.MultiClient.Net.Enums;
 using Fahrenheit.Atel;
 using Fahrenheit.FFX;
 using Fahrenheit.FFX.Battle;
 using Fahrenheit.Modules.ArchipelagoFFX.Client;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Text;
+
 using static Fahrenheit.FFX.Globals;
 using static Fahrenheit.Modules.ArchipelagoFFX.Client.FFXArchipelagoClient;
 using static Fahrenheit.Modules.ArchipelagoFFX.delegates;
@@ -14,6 +20,50 @@ namespace Fahrenheit.Modules.ArchipelagoFFX;
 
 [FhLoad(FhGameId.FFX)]
 public unsafe partial class CaptureModule : FhModule {
+    // Some structs that Fahrenheit is currently missing
+    [StructLayout(LayoutKind.Sequential)]
+    private struct BtlBinField {
+        [InlineArray(8)]
+        public struct FieldName {
+            private byte _data;
+        }
+
+        public short id;
+        public short data_offset;
+        public short __0x4;
+        public FieldName name;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x2)]
+    private struct BtlBinEncounter {
+        [FieldOffset(0x0)] public byte total_formation_count;
+        [FieldOffset(0x1)] public byte group_count;
+
+        [FieldOffset(0x2)] private BtlBinGroup _first_group;
+
+        [UnscopedRef]
+        public Span<BtlBinGroup> groups => MemoryMarshal.CreateSpan(ref _first_group, group_count);
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x5)]
+    private struct BtlBinGroup {
+        [FieldOffset(0x0)] public byte formation_count;
+        [FieldOffset(0x1)] public short battlefield;
+        [FieldOffset(0x3)] public byte grace;
+        [FieldOffset(0x4)] public byte total_weight;
+
+        [FieldOffset(0x5)] private BtlBinFormation _first_formation;
+
+        [UnscopedRef]
+        public Span<BtlBinFormation> formations => MemoryMarshal.CreateSpan(ref _first_formation, formation_count);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct BtlBinFormation {
+        public byte index;
+        public byte weight;
+    }
+
     private FhModContext? _mod_context;
     private FileStream? _global_state;
 
@@ -28,6 +78,7 @@ public unsafe partial class CaptureModule : FhModule {
         _MsSetRamChrParam = new FhMethodHandle<MsSetRamChrParam>(this, GAME, __addr_MsSetRamChrParam, h_MsSetRamChrParam);
         _MsSetSaveParam = new FhMethodHandle<MsSetSaveParam>(this, GAME, __addr_MsSetSaveParam, h_MsSetSaveParam);
         _MsCalcCommand = new FhMethodHandle<MsCalcCommand>(this, GAME, __addr_MsCalcCommand, h_MsCalcCommand);
+        _MsBattleEncountExe = new FhMethodHandle<MsBattleEncountExe>(this, GAME, __addr_MsBattleEncountExe, h_MsBattleEncountExe);
     }
 
     public override bool init(FhModContext mod_context, FileStream global_state_file) {
@@ -41,7 +92,8 @@ public unsafe partial class CaptureModule : FhModule {
             && _MsDamageCheckDeath.hook()
             && _MsSetRamChrParam.hook()
             && _MsSetSaveParam.hook()
-            && _MsCalcCommand.hook();
+            && _MsCalcCommand.hook()
+            && _MsBattleEncountExe.hook();
     }
 
     private static void set(byte* code_ptr, uint offset, AtelInst[] opcodes) {
@@ -100,7 +152,7 @@ public unsafe partial class CaptureModule : FhModule {
         231, // Dingo (Tutorial)
         232, // Water Flan (Tutorial)
         233, // Condor (Tutorial)
-        234, // Ragora (Tutorial)    
+        234, // Ragora (Tutorial)
     ];
     private void h_FUN_00783bb0(byte mon_idx) {
         byte num_initialized = FhUtil.get_at<byte>(0xD2CA80);
@@ -115,7 +167,7 @@ public unsafe partial class CaptureModule : FhModule {
             // Corrects incorrect monster arena indexes to be uncapturable
             if (_incorrect_arena_idx.Contains((short)(mon->chr_id & 0xFFF))) {
                 stats->monster_arena_idx = 0xFF;
-            }                
+            }
         }
         else {
             _logger.Debug($"{mon_idx}: already initialized ({mon->chr_id})");
@@ -246,5 +298,416 @@ public unsafe partial class CaptureModule : FhModule {
             }
         }
     }
-}
 
+    private int get_monster_arena_idx(int monster_id) {
+        byte[] filename = Encoding.UTF8.GetBytes($"host0:/ffx/master/jppc/battle/mon/_m{monster_id:D3}/m{monster_id:D3}.bin");
+
+        fixed (byte* filename_ptr = &filename[0]) {
+            void* filestream = _sceOpen(filename_ptr, 1);
+
+            if (filestream is null) {
+                _logger.Info("Failed to open monster file");
+                return -1;
+            }
+
+            int filesize = _sceLseek(filestream, 0, 2);
+            _sceLseek(filestream, 0, 0);
+
+            void* file = NativeMemory.Alloc((nuint)filesize);
+            _sceRead(filestream, file, filesize);
+            _sceClose(filestream);
+
+            int stats_offset = *(int*)((int)file + 0xC);
+            MonStats* mon_stats = (MonStats*)((int)file + stats_offset);
+
+            // Invalid idx
+            // ushort so we only have to check >= 512, not the < 0 case
+            if (mon_stats->monster_arena_idx >= 512 || mon_stats->monster_arena_idx == 0xFF) return -1;
+            int arena_idx = mon_stats->monster_arena_idx;
+
+            NativeMemory.Free(file);
+
+            return arena_idx;
+        }
+    }
+
+    private static readonly int[] species_capture_requirements = [
+        3, // Raldo
+        3, // Bunyip
+        3, // Murussu
+        3, // Mafdet
+        3, // Shred
+
+        4, // Gandarewa
+        4, // Aerouge
+        4, // Imp
+
+        3, // Dingo
+        3, // Mi'ihen Fang
+        3, // Garm
+        3, // Snow Wolf
+        3, // Sand Wolf
+        3, // Skoll
+        3, // Bandersnatch
+
+        3, // Water Flan
+        3, // Thunder Flan
+        3, // Snow Flan
+        3, // Ice Flan
+        3, // Flame Flan
+        3, // Dark Flan
+
+        3, // Dinonix
+        3, // Ipiria
+        3, // Raptor
+        3, // Melusine
+        3, // Iguion
+        3, // Yowie
+
+        5, // Condor
+        5, // Simurgh
+        5, // Alcyone
+
+        4, // Killer Bee
+        4, // Bite Bug
+        4, // Wasp
+        4, // Nebiros
+
+        4, // Floating Eye
+        4, // Buer
+        4, // Evil Eye
+        4, // Ahriman
+
+        1, // Ragora
+        1, // Grat
+
+        1, // Garuda
+        1, // Zu
+
+        1, // Sand Worm
+
+        0, // Unused
+
+        1, // Ghost
+
+        1, // Achelous
+        1, // Maelspike
+
+        5, // Dualhorn
+        5, // Valaha
+        5, // Grendel
+
+        5, // Vouivre
+        5, // Lamashtu
+        5, // Kusariqqu
+        5, // Mushussu
+        5, // Nidhogg
+
+        1, // Malboro
+        1, // Great Malboro
+
+        1, // Ogre
+        1, // Bashura
+
+        0, // Unused
+
+        1, // Splasher
+
+        3, // Yellow Element
+        3, // White Element
+        3, // Red Element
+        3, // Gold Element
+        3, // Blue Element
+        3, // Dark Element
+        3, // Black Element
+
+        1, // Epaaj
+
+        1, // Behemoth
+        1, // Behemoth King
+
+        1, // Chimera
+        1, // Chimera Brain
+
+        1, // Coeurl
+        1, // Master Coeurl
+
+        1, // Demonolith
+
+        10, // Iron Giant
+        10, // Gemini (Sword)
+        10, // Gemini (Club)
+
+        1, // Basilisk
+        1, // Anacondaur
+
+        1, // Adamantoise
+
+        1, // Varuna
+
+        1, // Ochu
+        1, // Mandragora
+
+        5, // Bomb
+        5, // Grenade
+
+        1, // Qactuar
+        1, // Cactuar
+
+        1, // Larva
+
+        1, // Barbatos
+
+        5, // Funguar
+        5, // Thorn
+        5, // Exoray
+
+        1, // Xiphos
+
+        5, // Puroboros
+
+        1, // Spirit
+
+        1, // Wraith
+
+        1, // Tonberry
+        1, // MastertTonberry
+
+        3, // Zaurus
+
+        3, // Halma
+
+        4, // Floating Death
+
+        1, // Machea
+    ];
+
+    private int get_monster_capture_requirement(int monster_arena_idx) {
+        return ArchipelagoFFXModule.seed.Options.CaptureRequirement switch {
+            ArchipelagoData.CaptureRequirement.None     => 0,
+            ArchipelagoData.CaptureRequirement.Area     => 1,
+            ArchipelagoData.CaptureRequirement.Species  => species_capture_requirements[monster_arena_idx],
+            ArchipelagoData.CaptureRequirement.Original => 10,
+            _ => throw new NotImplementedException(),
+        };
+    }
+
+    private int get_monster_captures_left(int monster_arena_idx) {
+        int capture_count = save_data->monsters_captured[monster_arena_idx];
+        int capture_req = get_monster_capture_requirement(monster_arena_idx);
+
+        return int.Max(0, capture_req - capture_count);
+    }
+
+    private int get_extra_weight_for_formation(BtlBinField* field, BtlBinFormation formation) {
+        char[] field_name = new char[6];
+
+        for (int i = 0; i < 6; i++) {
+            field_name[i] = (char)field->name[i];
+        }
+
+        string file_name = $"{new string(field_name)}_{formation.index:D2}";
+
+        byte[] filepath = Encoding.UTF8.GetBytes($"host0:/ffx/master/jppc/battle/btl/{file_name}/{file_name}.bin");
+
+        float extra_weight = 0.0f;
+
+        fixed (byte* filepath_ptr = &filepath[0]) {
+            void* open_result = _sceOpen(filepath_ptr, 1);
+
+            if (open_result is null) return 0;
+
+            int file_size = _sceLseek(open_result, 0, 2);
+            _sceLseek(open_result, 0, 0);
+
+            void* file = NativeMemory.Alloc((nuint)file_size);
+            _sceRead(open_result, file, file_size);
+            _sceClose(open_result);
+
+            int chunk_ptr = *(int*)((nint)file + 0xC);
+            if (chunk_ptr != 0) {
+                short* mon_ids_ptr = (short*)((nint)file + chunk_ptr + 0xC);
+                List<short> mon_ids = new(8);
+
+                for (int i = 0; i < 8; i++) {
+                    short mon_id = mon_ids_ptr[i];
+                    if (mon_id == -1) continue;
+
+                    mon_ids.Add((short)(mon_id & 0xFFF));
+                }
+
+                float[] weight_mults = new float[mon_ids.Count];
+
+                for (int i = 0; i < mon_ids.Count; i++) {
+                    int arena_idx = get_monster_arena_idx(mon_ids[i]);
+
+                    if (arena_idx == -1) {
+                        weight_mults[i] = 0.0f;
+                    } else {
+                        weight_mults[i] = (float)get_monster_captures_left(arena_idx)/get_monster_capture_requirement(arena_idx);
+                    }
+                }
+
+                bool all_captured = true;
+                foreach (float mult in weight_mults) {
+                    extra_weight += ArchipelagoFFXModule.seed.Options.EncounterWeighting * mult;
+                    if (mult > 0.0f) all_captured = false;
+                }
+
+                if (all_captured) {
+                    extra_weight = -2;
+                }
+
+                // Magic Urn
+                if (mon_ids.Contains(202)) {
+                    extra_weight = -1;
+                }
+            }
+
+            NativeMemory.Free(file);
+        }
+        return (int)extra_weight;
+    }
+
+    private int h_MsBattleEncountExe(int field_id, int group_idx, float walked_delta) {
+        // Globals
+        int* g_keybattle = FhUtil.ptr_at<int>(0xD2CA24);
+        int* g_keydown_R = FhUtil.ptr_at<int>(0xD2CA20);
+        int* DAT_0112ca28 = FhUtil.ptr_at<int>(0xD2CA28);
+        int* g_encounter_level = FhUtil.ptr_at<int>(0x8421C8);
+        int* EnableBattle = FhUtil.ptr_at<int>(0x8421BC);
+
+        // _logger.Info($"  g_keybattle = {*g_keybattle}");
+        // _logger.Info($"  g_keydown_R = {*g_keydown_R}");
+        // _logger.Info($"  DAT_0112ca28 = {*DAT_0112ca28}");
+        // _logger.Info($"  g_encounter_level = {*g_encounter_level}");
+        // _logger.Info($"  EnableBattle = {*EnableBattle}");
+
+        int field_idx = _MsBtlListFieldNum(field_id);
+
+        if (*g_keybattle != 0 && *g_keydown_R != 0) {
+            Battle.btl->group_idx = (byte)group_idx;
+            Battle.btl->formation_idx = (byte)*DAT_0112ca28;
+            *(byte*)((nint)Battle.btl + 0x12) = 1;
+            Battle.btl->field_idx = (ushort)field_idx;
+            _ResetEncountExe(1);
+            return -1;
+        }
+
+        bool can_encounter = true;
+        if (*g_encounter_level == 2) { // x4 encounter booster
+            walked_delta *= 10.0f;
+            can_encounter = true;
+        } else if (*g_encounter_level == 1) {
+            can_encounter = *(bool*)((nint)Battle.btl + 0x107);
+        } else if (*g_encounter_level == 0) {
+            walked_delta = 0.0f;
+            can_encounter = false;
+        }
+
+        // _logger.Info($"  btl.__0x12: {*(byte*)((nint)Battle.btl + 0x12)}");
+        // _logger.Info($"  can_encounter: {can_encounter}");
+
+        if (*(byte*)((nint)Battle.btl + 0x12) == 2) return 0;
+        if (field_idx < 0) return 0;
+        if (walked_delta <= 0.0f || !can_encounter) return 0;
+
+        BtlBinField* field = _MsBtlListField(field_idx);
+        BtlBinEncounter* encounter = _MsBtlListEncount(field_idx);
+
+        // _logger.Info($"  encounter->group_count: {encounter->group_count}");
+
+        if (group_idx < 0 || group_idx >= encounter->group_count) return 0;
+        if (*EnableBattle == 0) return 0;
+
+        BtlBinGroup* group = _MsBtlListGroup(field_idx, group_idx);
+
+        if (group->grace == 0 || group->total_weight == 0 || group->formation_count == 0) return 0;
+
+        Battle.btl->walked_dist += walked_delta;
+        Battle.btl->walked_dist_total += walked_delta;
+        Battle.btl->grace = group->grace;
+
+        int rolls_so_far = (int)(Battle.btl->walked_dist_total / 10.0f);
+
+        bool in_grace = group->grace / 2 >= rolls_so_far;
+
+        if (Battle.btl->walked_dist <= 10.0f) return 0;
+
+        for (; 10.0f < Battle.btl->walked_dist; Battle.btl->walked_dist -= 10.0f) {
+            if (in_grace) continue;
+
+            int encounter_chance = (rolls_so_far - (group->grace / 2)) * 256 / (group->grace * 4);
+            *(float*)((nint)Battle.btl + 0x110) *= 256.0f - encounter_chance;
+            *(float*)((nint)Battle.btl + 0x114) *= 256.0f;
+
+            if ((_brnd(0) & 0xFF) >= encounter_chance) continue;
+
+            // We have to prepare weights first
+            int total_weight = group->total_weight;
+            int[] weights = new int[group->formation_count];
+            bool all_complete = true;
+
+            for (int formation_idx = 0; formation_idx < group->formation_count; formation_idx++) {
+                BtlBinFormation formation = group->formations[formation_idx];
+
+                if (formation.weight == 0) {
+                    weights[formation_idx] = 0;
+                    continue;
+                }
+
+                weights[formation_idx] = formation.weight / 17;
+
+                int extra_weight = get_extra_weight_for_formation(field, formation);
+
+                if (extra_weight == -1) {
+                    weights[formation_idx] = 0;
+                    total_weight -= formation.weight / 17;
+                } else if (extra_weight == -2) {
+                    int old_weight = weights[formation_idx];
+                    weights[formation_idx] = 1;
+                    total_weight -= int.Max(0, old_weight - 1);
+                } else {
+                    weights[formation_idx] += extra_weight;
+                    total_weight += extra_weight;
+                    all_complete = false;
+                }
+            }
+
+            // Adjust weights if all formations met their requirements
+            if (all_complete)  {
+                total_weight = group->total_weight;
+
+                for (int formation_idx = 0; formation_idx < group->formation_count; formation_idx++) {
+                    BtlBinFormation formation = group->formations[formation_idx];
+                    weights[formation_idx] = formation.weight;
+                }
+            }
+
+            // Let's figure out which formation we should encounter!
+            int formation_rng = _brnd(1) % total_weight;
+            int iVar7 = 0;
+
+            for (int formation_idx = 0; formation_idx < group->formation_count; formation_idx++) {
+                iVar7 += weights[formation_idx];
+                if (!(formation_rng < iVar7)) continue;
+
+                if (*(byte*)((nint)Battle.btl + 0x27) == 0) {
+                    *(byte*)((nint)Battle.btl + 0x12) = 1;
+                } else {
+                    _Sg_FadeInW(3);
+                    save_data->battle_count += 1;
+                }
+
+                Battle.btl->field_idx = (ushort)field_idx;
+                Battle.btl->group_idx = (byte)group_idx;
+                Battle.btl->formation_idx = (byte)formation_idx;
+                _ResetEncountExe(1);
+
+                return -1;
+            }
+        }
+
+        return 0;
+    }
+}
