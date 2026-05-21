569d06bced79416b98145db12201efd585b68daa
Andrewki44 <andrewkilbourn1996@gmail.com>
2026-05-21 18:38:12 +1000

    add `ThunderPlainsProgressionFlags`

--- src/data/ArchipelagoData.cs
+++ src/data/ArchipelagoData.cs
@@ -811,7 +811,7 @@ public static class ArchipelagoData {
         {RegionEnum.ThunderPlains, new(){ story_progress = 1210, room_id = 140, entrance = 0, airship_destination_index = 10,
             savedata = [
                 new ArchipelagoRegionSaveData(0x03F8, 1, [0]),
-                new ArchipelagoRegionSaveData(0x03F9, 1, [0]),
+                new ArchipelagoRegionSaveData(0x03F1, 3, [0]) //save_data->progression_flags_thunder_plains
             ],
             story_checks = {
                 { 1375, new() {visit_complete = true, next_story_progress = 3210, next_room_id = 263, next_entrance = 2, return_to_airship = true, check_delegate = (r) => {ArchipelagoFFXModule.logger.Info("Thunder Plains visit complete"); } } },
